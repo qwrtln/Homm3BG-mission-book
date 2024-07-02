0e4e855f2531d124095f05e99c01a642d7a9557a
qwrtln <qwrtln@users.noreply.github.com>
2024-07-02 10:59:45 +0200

    v0.1 adjustments (#40)

--- tools/release.sh
+++ tools/release.sh
@@ -10,9 +10,6 @@ FILE_VERSION=$(echo "${VERSION}" | tr . _)
 declare -A languages=(
   ["en"]="English"
   ["pl"]="Polski"
-  ["es"]="Espanol"
-  ["fr"]="Francais"
-  ["ua"]="Ukrainska"
 )
 
 echo "Building release ${VERSION} for ${languages[$LANGUAGE]}..."
@@ -24,22 +21,6 @@ tools/build.sh ${LANGUAGE} &> /dev/null
 echo "Please inspect the PDF file."
 echo "Optimizing digital build..."
 tools/optimize.sh ${LANGUAGE} &> /dev/null
-mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}.pdf
-
-echo "Building printable version for ${languages[$LANGUAGE]}..."
-tools/make_printable.sh ${LANGUAGE} &> /dev/null
-git restore metadata.tex sections/
-echo "Please inspect the PDF file."
-echo "Optimizing printable build..."
-tools/optimize.sh ${LANGUAGE} cmyk &> /dev/null
-mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}_Printable.pdf
-
-echo "Building economy printable version for ${languages[$LANGUAGE]}..."
-tools/make_printable.sh ${LANGUAGE} --no-bg &> /dev/null
-git restore metadata.tex sections/
-echo "Please inspect the PDF file."
-echo "Optimizing economy printable build..."
-tools/optimize.sh ${LANGUAGE} cmyk &> /dev/null
-mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}_Economy_Printable.pdf
+mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Fan_Made_Mission_Book_${FILE_VERSION}.pdf
 
 echo "Done."
