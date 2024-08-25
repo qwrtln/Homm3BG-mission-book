461a9452f3626482a9d7fb14e261326230d5b932
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-08-25 14:03:36 +0200

    Update clean script

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-rm -f * 2> /dev/null
-rm -rf cache
+find . -iname "*.aux" -delete
+rm -rf cache 2> /dev/null
+rm -f * draft-scenarios/* screenshots/* 2> /dev/null
 git restore \
   README.md \
-  index_style.ist \
   latexmkrc \
   main_cs.tex \
-  main_de.tex \
   main_en.tex \
-  main_es.tex \
-  main_fr.tex \
   main_pl.tex \
-  main_ru.tex \
-  main_ua.tex \
   metadata.tex \
-  po4a.cfg
+  mkdocs.yml \
+  po4a.cfg \
+  draft-scenarios
