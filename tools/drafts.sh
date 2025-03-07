b6f4b164b70247f85d6c4396061a880f1cdf43ab
qwrtln <qwrtln@users.noreply.github.com>
2025-03-07 23:01:02 +0100

    Handle windows in build scripts (#170)

--- tools/drafts.sh
+++ tools/drafts.sh
@@ -3,9 +3,10 @@
 case "$(uname -s)" in
     Darwin*)    open=open;;
     Linux*)     open=xdg-open;;
+    MINGW*|MSYS*|CYGWIN*)    open=start;;
 esac
 
-cd draft-scenarios
+cd draft-scenarios || exit
 rm -f drafts.aux && \
   latexmk -pdflua -shell-escape drafts.tex
 ${open} drafts.pdf &> /dev/null &
