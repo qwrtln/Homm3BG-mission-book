30ca8ee2d944cbcd8c87dbe27121aa841c111566
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2025-03-08 18:36:13 +0100

    Handle windows symlinks in the draft build script

--- tools/drafts.sh
+++ tools/drafts.sh
@@ -6,6 +6,17 @@ case "$(uname -s)" in
     MINGW*|MSYS*|CYGWIN*)    open=start;;
 esac
 
+# For Windows only - replace symlink with a copy
+if [[ "$(uname -s)" =~ ^(MINGW|MSYS|CYGWIN) ]]; then
+    trap 'git restore draft-scenarios/assets' EXIT
+
+    if [[ -L "draft-scenarios/assets" ]]; then
+        target=$(readlink "draft-scenarios/assets")
+        rm "draft-scenarios/assets"
+        cp -r "$target" "draft-scenarios/assets"
+    fi
+fi
+
 cd draft-scenarios || exit
 rm -f drafts.aux && \
   latexmk -pdflua -shell-escape drafts.tex
