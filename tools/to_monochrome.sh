8adba420ab5e2bd99a175e8cc59e91e15b3c6d07
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2025-03-29 17:13:45 +0100

    Monochromize further parts of the book

--- tools/to_monochrome.sh
+++ tools/to_monochrome.sh
@@ -12,39 +12,39 @@ trap 'rm -rf ${TEMP_DIR}' EXIT
 
 # Extract alpha channel to preserve transparency
 magick "$INPUT_IMAGE" -alpha extract "$ALPHA_FILE"
-
-# Step 1: Create binary mask
-magick "$INPUT_IMAGE" \
-  -colorspace HSL \
-  -channel R \
-  -separate +channel \
-  -threshold 60% \
-  -negate \
-  -morphology close square:3 \
-  -blur 0x3 \
-  -threshold 85% \
-  -morphology Close Diamond:3 \
-  -morphology Open Diamond:8 \
-  -morphology Close Octagon:32 \
-  "$BINARY_MASK"
-
-# Create inverted mask
-magick "$BINARY_MASK" -negate "$INVERTED_MASK"
-
-# Step 2: Create monochrome version
-magick "$INPUT_IMAGE" \
-  -negate \
-  -threshold 50% \
-  -negate \
-  -fill white -opaque black \
-  -morphology Dilate Diamond:2 \
-  -channel RGB -negate \
-  "$MONO_IMAGE"
-
-# Step 3: Create final composite image
-magick "$INPUT_IMAGE" "$BINARY_MASK" -compose CopyOpacity -composite "$COLOR_PARTS" &
-magick "$MONO_IMAGE" "$INVERTED_MASK" -compose CopyOpacity -composite "$MONO_PARTS" &
-wait
-magick "$COLOR_PARTS" "$MONO_PARTS" -compose Plus -composite \
-  "$ALPHA_FILE" -alpha off -compose CopyOpacity -composite \
-  "$INPUT_IMAGE"
+#
+# # Step 1: Create binary mask
+# magick "$INPUT_IMAGE" \
+#   -colorspace HSL \
+#   -channel R \
+#   -separate +channel \
+#   -threshold 60% \
+#   -negate \
+#   -morphology close square:3 \
+#   -blur 0x3 \
+#   -threshold 85% \
+#   -morphology Close Diamond:3 \
+#   -morphology Open Diamond:8 \
+#   -morphology Close Octagon:32 \
+#   "$BINARY_MASK"
+#
+# # Create inverted mask
+# magick "$BINARY_MASK" -negate "$INVERTED_MASK"
+#
+# # Step 2: Create monochrome version
+# magick "$INPUT_IMAGE" \
+#   -negate \
+#   -threshold 50% \
+#   -negate \
+#   -fill white -opaque black \
+#   -morphology Dilate Diamond:2 \
+#   -channel RGB -negate \
+#   "$MONO_IMAGE"
+#
+# # Step 3: Create final composite image
+# magick "$INPUT_IMAGE" "$BINARY_MASK" -compose CopyOpacity -composite "$COLOR_PARTS" &
+# magick "$MONO_IMAGE" "$INVERTED_MASK" -compose CopyOpacity -composite "$MONO_PARTS" &
+# wait
+# magick "$COLOR_PARTS" "$MONO_PARTS" -compose Plus -composite \
+#   "$ALPHA_FILE" -alpha off -compose CopyOpacity -composite \
+#   "$INPUT_IMAGE"
