ac91605c72c462b86c7fe3465630bd705337ef77
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2025-04-10 18:30:25 +0200

    Prepare scripts for docs

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 find . -iname "*.aux" -delete
+find . -type d -name "translated" -name "svg-inkscape" -exec rm -rf {} \; 2>/dev/null || true
 rm -rf cache 2> /dev/null
-rm -f * draft-scenarios/* screenshots/* 2> /dev/null
+rm -f ./* draft-scenarios/* screenshots/* 2> /dev/null
 git restore \
+  LICENSE \
   README.md \
+  draft-scenarios \
   latexmkrc \
   main_cs.tex \
+  main_de.tex \
   main_en.tex \
+  main_fr.tex \
   main_pl.tex \
   metadata.tex \
   mkdocs.yml \
-  po4a.cfg \
-  draft-scenarios
+  po4a.cfg
