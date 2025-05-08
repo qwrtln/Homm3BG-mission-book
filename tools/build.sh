381036a0c76c02a3b0a32f77947a9cbcbaabd7c8
qwrtln <qwrtln@users.noreply.github.com>
2025-05-08 21:42:10 +0200

    Update tools/build.sh
    Co-authored-by: Alexey Sokolov <alexey+github@asokolov.org>

--- tools/build.sh
+++ tools/build.sh
@@ -198,7 +198,7 @@ if [[ -n "${SCENARIO_SEARCH}" ]]; then
   if ! tools/_find_scenario.sh "${SCENARIO_SEARCH}"; then
     exit 1
   fi
-  SCENARIO=$(perl -ne 'while(/[^\/{}]*\.[^{}]*/g){$_=$&;s/\..*//;print "$_\n"}' structure.tex)
+  SCENARIO=$(perl -pE 's|.*/||; s|\..*||' structure.tex)
   # Handle monochrome mode for a single file
   if [[ "${HOMM3_NO_ART_BACKGROUND}" -eq 1 ]]; then
     CACHE_DIR="cache/monochrome-maps"
