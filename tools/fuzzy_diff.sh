eea761fbcf6739fe273b48db2ae3c8533e745713
Tomas Zeman <tomaas.zeman@gmail.com>
2025-08-23 08:47:03 +0200

    Make `head` POSIX-compliant

--- tools/fuzzy_diff.sh
+++ tools/fuzzy_diff.sh
@@ -25,7 +25,7 @@ trap 'rm -rf -- "$MYTMPDIR"' EXIT
 msgattrib --only-fuzzy "$1" > "$MYTMPDIR/fuzzy_only.po"
 awk '/^#, fuzzy/ {print; found=1; next} found {print; if (/^msgstr/) exit}' "$MYTMPDIR/fuzzy_only.po" > "$MYTMPDIR/fuzzy_first.po"
 awk '/^#\|/,/^msgid/ {if ($0 ~ /^#\|/) print substr($0, 4)}' "$MYTMPDIR/fuzzy_first.po" > "$MYTMPDIR/old.po"
-awk '/^msgid/,/^msgstr/ {print}' "$MYTMPDIR/fuzzy_first.po" | head -n -1 > "$MYTMPDIR/new.po"
+awk '/^msgid/,/^msgstr/ {print}' "$MYTMPDIR/fuzzy_first.po" | head -n 1 > "$MYTMPDIR/new.po"
 diff -u "$MYTMPDIR/old.po" "$MYTMPDIR/new.po" | delta \
   --no-gitconfig \
   --dark \
