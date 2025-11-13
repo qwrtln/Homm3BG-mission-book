cd2f56760c04978bd32178e0a89e86beed95e658
qwrtln <qwrtln@users.noreply.github.com>
2025-11-14 00:52:52 +0100

    Single scenario comparison (#375)

--- tools/compare_pages.sh
+++ tools/compare_pages.sh
@@ -6,14 +6,15 @@ output_dir="screenshots"
 
 help() {
   echo "
-    Usage: ./tools/compare_pages.sh (-l <language> | -d) -r <range> [OPTIONS]
+    Usage: ./tools/compare_pages.sh (-l <language> | -d | -s|--scenario SEARCH) -r <range> [OPTIONS]
 
     Mandatory Arguments (choose one):
       -l, --language <language>     Specify the language for comparison (en, pl, cs, de, fr).
-      -d, --drafts                  Compare draft scenarios (mutually exclusive with -l).
+      -d, --drafts                  Compare draft scenarios (mutually exclusive with -l and -s).
       -r, --range <range>           Provide comma-separated list of pages or range of pages you want to compare.
 
     Optional Arguments:
+      -s, --scenario                Compare against a single scenario.
       -p, --printable               Compares your build against 'printable' build.
       -s, --single-page             Combines all compared pages into a single image.
       -m, --mono                    Uses monochrome version of files for baseline comparison.
@@ -38,19 +39,18 @@ help() {
   exit 2
 }
 
-file_type() {
+_file_type() {
   local printable="$1"
   [[ "$printable" -eq 1 ]] && echo "printable" || echo "main"
 }
 
 base_file_path() {
-  local identifier="$1"
+  local language="$1"
   local printable="$2"
   local drafts="$3"
   local monochrome="$4"
   local type
   local file_suffix=""
-
   if [[ "$monochrome" -eq 1 ]]; then
     file_suffix="-mono"
   fi
@@ -58,13 +58,59 @@ base_file_path() {
   if [[ "$drafts" -eq 1 ]]; then
     echo "${cache_dir}/drafts${file_suffix}.pdf"
   else
-    type=$(file_type "$printable")
-    echo "${cache_dir}/${type}_${identifier}${file_suffix}.pdf"
+    type=$(_file_type "$printable")
+    echo "${cache_dir}/${type}_${language}${file_suffix}.pdf"
+  fi
+}
+
+scenario_file_path() {
+  local language="$1"
+  local scenario="$2"
+  local monochrome="$3"
+  local file_suffix=""
+
+  if [[ "$monochrome" -eq 1 ]]; then
+    file_suffix="_mono"
+  fi
+  find "${cache_dir}" -name "${scenario}_${language}${file_suffix}*"
+}
+
+_sha_from_filename() {
+  local filename="$1"
+  echo "$filename" | grep -oE '[0-9a-f]{40}'
+}
+
+_sha_from_remote() {
+  local git_branch="$1"
+  curl -s -f -H "Accept: application/vnd.github.VERSION.sha" "https://api.github.com/repos/qwrtln/Homm3BG-mission-book-build-artifacts/commits/$git_branch" 2>/dev/null
+}
+
+_remote_pdf_file_from_filename() {
+  local filename="$1"
+  local sha
+  sha=$(_sha_from_filename "$filename")
+  if [[ $sha == "" ]]; then
+    # No SHA - file is absent and its name was already constructed
+    echo "$filename"
+  else
+    # It is a local file with SHA
+    basename "${filename%_"$sha".pdf}.pdf"
+  fi
+}
+
+_git_branch_from_filename() {
+  local filename="$1"
+  remote_pdf_file=$(_remote_pdf_file_from_filename "$filename")
+  git_branch=""
+  if [[ "$remote_pdf_file" == *"_mono.pdf" ]]; then
+    echo "${LANGUAGE}-${remote_pdf_file%_"$LANGUAGE"_mono.pdf}-mono"
+  else
+    echo "${LANGUAGE}-${remote_pdf_file%_"$LANGUAGE".pdf}-color"
   fi
 }
 
 download_base_file() {
-  local identifier="$1"
+  local language="$1"
   local printable="$2"
   local drafts="$3"
   local monochrome="$4"
@@ -79,15 +125,47 @@ download_base_file() {
 
   if [[ "$drafts" -eq 1 ]]; then
     url="https://raw.githubusercontent.com/qwrtln/Homm3BG-mission-book-build-artifacts/drafts/drafts${file_suffix}.pdf"
-    output_file=$(base_file_path "$identifier" "$printable" "$drafts" "$monochrome")
+    output_file=$(base_file_path "$language" "$printable" "$drafts" "$monochrome")
   else
-    type=$(file_type "$printable")
-    url="https://raw.githubusercontent.com/qwrtln/Homm3BG-mission-book-build-artifacts/${identifier}/${type}_${identifier}${file_suffix}.pdf"
-    output_file=$(base_file_path "$identifier" "$printable" "$drafts" "$monochrome")
+    type=$(_file_type "$printable")
+    url="https://raw.githubusercontent.com/qwrtln/Homm3BG-mission-book-build-artifacts/${language}/${type}_${language}${file_suffix}.pdf"
+    output_file=$(base_file_path "$language" "$printable" "$drafts" "$monochrome")
   fi
 
   mkdir -p "$cache_dir"
   curl -o "$output_file" "$url"
+}
+
+download_scenario() {
+  local language="$1"
+  local filename="$2"
+  local monochrome="$3"
+  local file_suffix=""
+  if [[ "$monochrome" -eq 1 ]]; then
+    file_suffix="_mono"
+  fi
+  if [[ "$filename" == "" ]]; then
+    filename="${SCENARIO_NAME}_${LANGUAGE}${file_suffix}.pdf"
+  fi
+
+  local git_branch
+  local remote_sha
+  local remote_filename
+
+  git_branch=$(_git_branch_from_filename "$filename")
+  remote_sha=$(_sha_from_remote "$git_branch")
+  remote_filename=$(_remote_pdf_file_from_filename "$filename")
+
+  local url="https://raw.githubusercontent.com/qwrtln/Homm3BG-mission-book-build-artifacts/$git_branch/$remote_filename"
+  local output_filename="${SCENARIO_NAME}_${LANGUAGE}${file_suffix}_${remote_sha}.pdf"
+
+  mkdir -p "$cache_dir"
+  curl -o "$cache_dir/$output_filename" "$url"
+
+  # Ensure old file is deleted
+  find "$cache_dir" -maxdepth 1 -name "${SCENARIO_NAME}_${LANGUAGE}${file_suffix}*.pdf" ! -name "$output_filename" -delete &> /dev/null
+
+  echo "$cache_dir/$output_filename"
 }
 
 file_mod_time() {
@@ -131,18 +209,56 @@ is_pdf_current() {
   return 1
 }
 
-# Only download a base file if it's not already present locally or
-# is older than 1 hour. Otherwise we use the cached one to speed-up the workflow.
+is_scenario_current() {
+  local scenario_pdf_file="$1"
+
+  if [[ ! -f "$scenario_pdf_file" ]]; then
+    return 1
+  fi
+
+  # Extract commit SHA from cached PDF file name
+  scenario_git_sha=$(_sha_from_filename "$scenario_pdf_file")
+  # Extract remote PDF file name
+  remote_pdf_file=$(_remote_pdf_file_from_filename "$scenario_pdf_file")
+
+  # Construct git branch name from PDF file name and language
+  git_branch=$(_git_branch_from_filename "$scenario_pdf_file")
+  # Check remote commit SHA using GitHub API
+  local latest_sha
+  latest_sha=$(_sha_from_remote "$git_branch")
+
+  if [[ "$latest_sha" != "$scenario_git_sha" ]]; then
+    return 1
+  fi
+  return 0
+}
+
+# Only download a base files if they are not already present locally or don't match current git ref.
+# Otherwise we use the cached one to speed-up the workflow.
 ensure_base_file() {
   local base_file
-  local identifier="$1"
+  local language="$1"
   local printable="$2"
   local drafts="$3"
   local monochrome="$4"
-  local base_file=$(base_file_path "$identifier" "$printable" "$drafts" "$monochrome")
+  base_file=$(base_file_path "$language" "$printable" "$drafts" "$monochrome")
 
   if ! is_pdf_current "$base_file"; then
     download_base_file "$language" "$printable" "$drafts" "$monochrome"
+  fi
+
+  echo "$base_file"
+}
+
+ensure_scenario_file() {
+  local base_file
+  local language="$1"
+  local scenario="$2"
+  local monochrome="$3"
+  base_file=$(scenario_file_path "$language" "$scenario" "$monochrome")
+
+  if [[ "$base_file" == "" ]] || ! is_scenario_current "$base_file"; then
+    base_file=$(download_scenario "$language" "$base_file" "$monochrome")
   fi
 
   echo "$base_file"
@@ -176,34 +292,40 @@ parse_pages() {
 # MAIN FLOW
 #
 
-language=""
-range=""
-printable=0
-single_page=0
-drafts=0
-monochrome=0
+LANGUAGE=""
+RANGE=""
+PRINTABLE=0
+DRAFTS=0
+MONOCHROME=0
+SCENARIO_SEARCH=""
+SCENARIO_NAME=""
 
 while [[ "$1" != "" ]]; do
   case $1 in
     -l | --language )
       shift
-      language=$1
+      LANGUAGE=$1
       ;;
     -d | --drafts )
-      drafts=1
+      DRAFTS=1
       ;;
     -p | --printable )
-      printable=1
+      PRINTABLE=1
       ;;
     -r | --range )
       shift
-      range=$1
+      RANGE=$1
       ;;
-    -s | --single-page )
-      single_page=1
+    -s | --scenario )
+      shift
+      if [[ $# -lt 1 ]]; then
+        echo "Error: --scenario requires a string argument" >&2
+        usage
+      fi
+      SCENARIO_SEARCH="$1"
       ;;
     -m | --mono )
-      monochrome=1
+      MONOCHROME=1
       ;;
     * )
       help
@@ -213,65 +335,75 @@ while [[ "$1" != "" ]]; do
 done
 
 # Check that we have either language or draft but not both
-if [[ "$drafts" -eq 1 && -n "$language" ]]; then
-  echo "Error: -d/--drafts and -l/--language options are mutually exclusive."
+if [[ "$DRAFTS" -eq 1 && (-n "$LANGUAGE" || -n "$scenario") ]]; then
+  echo "Error: -d/--drafts is mutually exclusive with -l/--language and -s/--scenario options."
   help
 fi
 
-# Check that we have either language or draft
-if [[ "$drafts" -eq 0 && -z "$language" ]]; then
-  echo "Error: You must specify either -l/--language or -d/--drafts option."
-  help
-fi
-
-if [[ -z "$range" ]]; then
+if [[ -z "$RANGE" ]]; then
   echo "Error: You must specify a page range with -r/--range option."
   help
 fi
 
+if [[ -z "$LANGUAGE" ]]; then
+  LANGUAGE="en"
+fi
+
+if [[ "$SCENARIO_SEARCH" ]]; then
+  tools/_find_scenario.sh "$SCENARIO_SEARCH"
+  SCENARIO_NAME=$(grep -o "/[a-z_]*\.tex" structure.tex | sed -e "s@/@@" -e "s@.tex@@")
+  git restore structure.tex
+fi
+
 # Set the identifier for file paths and naming
-identifier="$language"
-if [[ "$drafts" -eq 1 ]]; then
+identifier="$LANGUAGE"
+if [[ "$DRAFTS" -eq 1 ]]; then
   identifier="drafts"
   # Printable option doesn't apply to drafts
-  if [[ "$printable" -eq 1 ]]; then
+  if [[ "$PRINTABLE" -eq 1 ]]; then
     echo "Note: --printable option ignored for drafts."
-    printable=0
+    PRINTABLE=0
   fi
 fi
 
+base_file=""
 echo "Checking if there is the base file for comparison..."
-base_file=$(ensure_base_file "$language" "$printable" "$drafts" "$monochrome")
+if [[ "$SCENARIO_NAME" != "" ]]; then
+  base_file=$(ensure_scenario_file "$LANGUAGE" "$SCENARIO_NAME" "$MONOCHROME")
+else
+  base_file=$(ensure_base_file "$LANGUAGE" "$PRINTABLE" "$DRAFTS" "$MONOCHROME")
+fi
 
 tmp_dir="$(mktemp -d)"
 trap 'rm -rf -- "$tmp_dir"' EXIT
 
-readarray -t pages < <(parse_pages "$range")
+readarray -t pages < <(parse_pages "$RANGE")
 
 for page in "${pages[@]}"; do
-  echo "Making images of ${base_file} and $([ "$drafts" -eq 1 ] && echo "drafts.pdf" || echo "main_${language}.pdf") for page ${page}..."
-  pdftoppm "${base_file}" "${tmp_dir}/aa" -f "${page}" -l "${page}" -png -progress &
+  echo "Making images of ${base_file} and $([ "$DRAFTS" -eq 1 ] && echo "drafts.pdf" || [ "$SCENARIO_NAME" != "" ] && echo "${SCENARIO_NAME}.pdf" || echo "main_${LANGUAGE}.pdf") for page ${page}..."
+  pdftoppm "${base_file}" "${tmp_dir}/aa-$(printf %02d "$page")" -f "${page}" -l "${page}" -png -progress -singlefile &
 
-  if [[ "$drafts" -eq 1 ]]; then
-    pdftoppm "draft-scenarios/drafts.pdf" "${tmp_dir}/bb" -f "${page}" -l "${page}" -png -progress &
+  if [[ "$DRAFTS" -eq 1 ]]; then
+    pdftoppm "draft-scenarios/drafts.pdf" "${tmp_dir}/bb-$(printf %02d "$page")" -f "${page}" -l "${page}" -png -progress -singlefile &
+  elif [[ "$SCENARIO_NAME" != "" ]]; then
+    pdftoppm "${SCENARIO_NAME}.pdf" "${tmp_dir}/bb-$(printf %02d "$page")" -f "${page}" -l "${page}" -png -progress -singlefile &
   else
-    pdftoppm "main_${language}.pdf" "${tmp_dir}/bb" -f "${page}" -l "${page}" -png -progress &
+    pdftoppm "main_${LANGUAGE}.pdf" "${tmp_dir}/bb-$(printf %02d "$page")" -f "${page}" -l "${page}" -png -progress -singlefile &
   fi
 done
 
 wait
 
+if [[ "$SCENARIO_NAME" != "" ]]; then
+  identifier="$SCENARIO_NAME"
+fi
 for page in "${pages[@]}"; do
   echo "Combining pages $(printf %02d "$page")..."
+  echo "${tmp_dir}"/*"$(printf %02d "$page")".png
+  echo "${tmp_dir}/${identifier}-$(printf %02d "$page").png"
   montage "${tmp_dir}"/*"$(printf %02d "$page")".png -tile 2x1 -geometry +0+0 "${tmp_dir}/${identifier}-$(printf %02d "$page").png" && \
   rm "${tmp_dir}/aa-$(printf %02d "$page").png" "${tmp_dir}/bb-$(printf %02d "$page").png" &
 done
-
-if [[ "$single_page" -eq 1 ]]; then
-  wait
-  montage "${tmp_dir}/${identifier}"* -tile "1x" -geometry +0+0 "${tmp_dir}/${identifier}-all.png"
-fi
-
 wait
 
 mkdir -p screenshots
